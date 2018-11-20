749c1ac823f855cdd3ebfe0fe251380005096dff
Tomas Veras <tomasverasm@gmail.com>
2018-11-20 02:10:02 -0400

    feat: deleting parties, games and keepAlives without players.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const purgeParties = functions.firestore
+  .document("parties/{partyId}")
+  .onWrite(async (change, context) => {
+    if (!change.after.exists) return Promise.resolve("skipped, deleted");
+
+    const { players = {} } = change.after.data();
+    if (Object.keys(players).length) return null;
+
+    const db = admin.firestore();
+
+    await db
+      .collection("keepAlive")
+      .doc(context.params.partyId)
+      .delete();
+
+    await db
+      .collection("games")
+      .doc(context.params.partyId)
+      .delete();
+    return change.after.ref.delete();
+  });
